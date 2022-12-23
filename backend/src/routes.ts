c6797b453c9380b54be4987b18b024b1fbf75862
L-edu <edurei75@gmail.com>
2022-12-23 19:45:23 -0200

    aula 21/12 - parte 02

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,37 @@
-import {Router} from 'express';
-import { AuthUserController } from './controllers/user/AuthUserController';
-import { CreateUserController } from './controllers/user/CreateUserController';
-import { DetailUserController } from './controllers/user/DetailUserController';
-import {isAuthenticated} from './middlewares/isAuthenticated';
+import { Router } from 'express';
+import multer from 'multer';
+
+import { CreateUserController } from './controllers/user/CreateUserController'
+import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
+
+import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
+
+import { CreateProductController } from './controllers/product/CreateProductController'
+
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
+import uploadConfig from './config/multer'
 
 const router = Router();
 
-router.post('/users', new CreateUserController().handle)
-router.post('/session', new AuthUserController().handle)
-router.get('/me', isAuthenticated, new DetailUserController().handle)
+const upload = multer(uploadConfig.upload("./tmp"));
 
-export {router};
+//-- ROTAS USER --
+router.post('/users', new CreateUserController().handle)
+
+router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated,  new DetailUserController().handle )
+
+//-- ROTAS CATEGORY
+router.post('/category', isAuthenticated, new CreateCategoryController().handle )
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle )
+
+//-- ROTAS PRODUCT
+router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle )
+
+
+export { router }; 
